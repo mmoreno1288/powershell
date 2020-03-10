5c48119a26ce7da15353b6c678c8e06a6102c4f7
Matthew Moreno <mmoreno1288@gmail.com>
2020-03-10 15:45:47 -0700

    Create errorhandling.ps1

---
+++ errorhandling.ps1
@@ -0,0 +1,22 @@
+﻿$a = 0
+$inputOK = $false
+do
+{
+  try
+  {
+    $a = Read-Host "Please enter the number between 1 and 4"
+    if (([int]$a)) {
+    $inputOK = $true}
+  }
+  catch
+  {
+    Write-Host -ForegroundColor red "INVALID INPUT!  Please enter a numeric value."
+  } 
+
+}
+until ($inputOK)
+
+Write-Host $a.GetType().Name
+$a_int = [int]$a
+Write-Host $a_int.GetType().Name
+Write-Host –ForegroundColor green "You have entered $a"
